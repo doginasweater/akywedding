f8ff0c79b048ccf0a5a80c8f804b3e2d0a0bbd98
Kevin Bernfeld <kcbernfeld@gmail.com>
2022-07-20 16:01:46 -0400

    initial setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    'import',
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended'
+  ],
+  rules: {
+    "array-bracket-spacing": ["warn", "always"],
+    "eqeqeq": ["error", "smart"],
+    "jsx-quotes": ["warn", "prefer-double"],
+    "no-nested-ternary": "error",
+    "object-curly-spacing": ["warn", "always"],
+    "object-property-newline": ["warn", { "allowAllPropertiesOnSameLine": true }],
+    "prefer-const": "error",
+    "@typescript-eslint/quotes": ["warn", "single", { "avoidEscape": true }],
+    "@typescript-eslint/semi": ["error", "always"],
+    "import/no-default-export": "error",
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  }
+};
