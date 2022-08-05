09e6456f158f9500fbcf8487f08e18a112840391
Kevin Bernfeld <kcbernfeld@gmail.com>
2022-08-05 13:03:37 -0400

    tweaking eslint, no more no unescaped entities

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,10 @@
+/* eslint-disable no-undef */
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
-    project: ['./tsconfig.json'],
+    project: [ './tsconfig.eslint.json' ],
   },
   plugins: [
     'import',
@@ -18,17 +19,18 @@ module.exports = {
     'plugin:react-hooks/recommended'
   ],
   rules: {
-    "array-bracket-spacing": ["warn", "always"],
-    "eqeqeq": ["error", "smart"],
-    "jsx-quotes": ["warn", "prefer-double"],
-    "no-nested-ternary": "error",
-    "object-curly-spacing": ["warn", "always"],
-    "object-property-newline": ["warn", { "allowAllPropertiesOnSameLine": true }],
-    "prefer-const": "error",
-    "@typescript-eslint/quotes": ["warn", "single", { "avoidEscape": true }],
-    "@typescript-eslint/semi": ["error", "always"],
-    "import/no-default-export": "error",
-    "react/prop-types": "off"
+    'array-bracket-spacing': [ 'warn', 'always' ],
+    'eqeqeq': [ 'error', 'smart' ],
+    'jsx-quotes': [ 'warn', 'prefer-double' ],
+    'no-nested-ternary': 'error',
+    'object-curly-spacing': [ 'warn', 'always' ],
+    'object-property-newline': [ 'warn', { 'allowAllPropertiesOnSameLine': true } ],
+    'prefer-const': 'error',
+    '@typescript-eslint/quotes': [ 'warn', 'single', { 'avoidEscape': true } ],
+    '@typescript-eslint/semi': [ 'error', 'always' ],
+    'import/no-default-export': 'error',
+    'react/no-unescaped-entities': 'off',
+    'react/prop-types': 'off'
   },
   settings: {
     react: {
