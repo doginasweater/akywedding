7f80aa3072ff881222182c5edb7891e3d0047157
Aky Mac Donald <akymacd@gmail.com>
2022-08-11 23:25:05 -0400

    Header styling version 1

--- src/components/TextAreaField.tsx
+++ src/components/TextAreaField.tsx
@@ -2,14 +2,14 @@ import {
   FormControl,
   FormErrorMessage,
   FormLabel,
-} from "@chakra-ui/form-control";
+} from '@chakra-ui/form-control';
 import { Textarea } from '@chakra-ui/react';
-import { Field, useField } from "formik";
+import { Field, useField } from 'formik';
 
 
 //reusable text input component
 export const TextAreaField = ({ label, ...props }) => {
-  const [field, meta] = useField(props);
+  const [ field, meta ] = useField(props);
 
   return (
     <FormControl isInvalid={meta.error && meta.touched}>
--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -5,20 +5,20 @@ export const Header: React.FC = () => {
   return (
     <div className="header">
       <nav>
-        <ul className="ul-items">
-          <li className="li-items">
+        <ul className="link-group">
+          <li className="link-items">
             <Link to="/">Home</Link>
 
             <Link to="/travel">Travel</Link>
 
           </li>
         </ul>
-        <div className="logo">
-          <h1>Aky&amp;Andrew</h1>
+        <div className="header-title">
+          <h1>A &amp; A</h1>
         </div>
         <div className="right">
-          <ul className="ul-items">
-            <li className="li-items">
+          <ul className="link-group">
+            <li className="link-items">
             <Link to="/registry">Registry</Link>
             <Link to="/rsvp">RSVP</Link>
             </li>
