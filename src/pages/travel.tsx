6617b35688252a4dfa3d124e8d22b660eeb4d8d0
Aky Mac Donald <akymacd@gmail.com>
2022-08-13 13:43:03 -0400

    Styled Registry with image

--- src/pages/travel.tsx
+++ src/pages/travel.tsx
@@ -5,8 +5,8 @@ import {
 
 export const Travel = () => {
   return (
-    <main>
-      <section>
+
+
         <article className="travel">
           <h2>Travel</h2>
         <h3>Getting to Niagara</h3>
@@ -16,9 +16,9 @@ export const Travel = () => {
         <h3>Getting to Cave Spring Vineyard</h3>
         <p>The ceremony at Cave Spring Vineyard is about a 10 minute drive from the reception at Inn on the Twenty. Alternative forms of transportation are not as widely available, so carpooling is encouraged.</p>
         
-      </article>  
+
       
-      <article className="hotels">
+
         <h2>Hotels &amp; Accommodations</h2>
         <h3>Hotels</h3>
           <p><Link href="https://www.vintage-hotels.com/jordan-house/" isExternal color="grey.500">Jordan House</Link> and <Link href="https://www.vintage-hotels.com/inn-on-the-twenty/" isExternal color="grey.500">Inn on the Twenty</Link> offer accommodations directly at the reception site. However, space is limited. Nearby options include:</p>
@@ -36,15 +36,15 @@ export const Travel = () => {
               <Link href="https://www.capriinn.com/" isExternal color="grey.500">Capri Inn St. Catharines</Link>
           </li>
             <li><Link href="https://www.hilton.com/en/hotels/ycmmbhx-hampton-st-catharines-niagara/" isExternal color="grey.500">Hampton Inn St. Catharines</Link></li>
-            <li><Link href="https://www.choicehotels.com/en-ca/ontario/st-catharines/comfort-inn-hotels/cn303" isExternal color="grey.500">Confort Inn St. Catharines</Link></li>
+            <li><Link href="https://www.choicehotels.com/en-ca/ontario/st-catharines/comfort-inn-hotels/cn303" isExternal color="grey.500">Comfort Inn St. Catharines</Link></li>
           </ul>
         
         <h2>Other Accommodations</h2>
           <p><Link href="https://www.airbnb.ca/s/Jordan--Lincoln--ON--Canada/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&place_id=ChIJCdXisahS04kRksOJhprJPuw&date_picker_type=calendar&checkin=2022-10-16&checkout=2022-10-17&source=structured_search_input_header&search_type=autocomplete_click" isExternal color="grey.500">Airbnb</Link> is another option, especially for large groups or if you plan to spend extra time enjoying the Niagara region. Search for "Jordan, Lincoln, ON" (or click the link—I've done it for you!)</p>
 
-      </article>
 
-      <article className="transport">
+
+
         <h2>Transportation</h2>
 
         <h3>Carpooling</h3>
@@ -52,9 +52,9 @@ export const Travel = () => {
 
         <h3>Uber &amp; Taxis</h3>
         <p>The area has uber service to help you get back to your accommodations safely. Taxis are also available out of St. Catharines, but need to be ordered ahead. Plan ahead to end the night safely, and consider sharing a ride with other guests.</p>
-      </article>
+
       
-      </section>
-      </main>
+</article>
+
   );
 };
