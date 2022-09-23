6c73fe57c56a6b8de2112179df047c13259385af
Aky Mac Donald <akymacd@gmail.com>
2022-09-23 14:53:41 -0400

    Added updated travel info

--- src/pages/registry.tsx
+++ src/pages/registry.tsx
@@ -13,7 +13,7 @@ export const Registry = () => {
       </div>
       <section className="registry">
         <h2>Registry</h2>
-        <p>We are finally taking our dream vacation—previously scheduled for March 2020 and cancelled due to the pandemic—to Japan in autumn 2022 for our honeymoon. We would be so grateful if you would consider contributing to <a href="https://www.honeyfund.com/wedding/mac-donald-komar-10-17-2021" target="blank">our honeymoon fund via Honeyfund</a>.</p>
+        <p>We are finally taking our dream vacation—previously scheduled for March 2020 and cancelled due to the pandemic—to Japan in spring 2023 for our honeymoon. We would be so grateful if you would consider contributing to <a href="https://www.honeyfund.com/wedding/mac-donald-komar-10-17-2021" target="blank">our honeymoon fund via Honeyfund</a>.</p>
         <p>If you would prefer to give us a more conventional gift we can enjoy at home rather than away, we have also made a <a href="https://registry.thebay.com/registry/view-registry/akiandrew2021" target="blank">traditional registry at HBC</a>. Feel free to go rogue using this registry as inspiration.</p>
 
         <a href="https://www.honeyfund.com/wedding/mac-donald-komar-10-17-2021" target="blank">
